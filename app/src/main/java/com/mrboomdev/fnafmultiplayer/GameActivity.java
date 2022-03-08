c3f15229d97150d020931bb86d35419fb2f4080e
MrBoomDeveloper <mrboomsocial@gmail.com>
2022-03-08 16:55:30 +0500

    created repository

---
+++ app/src/main/java/com/mrboomdev/fnafmultiplayer/GameActivity.java
@@ -0,0 +1,13 @@
+package com.mrboomdev.fnafmultiplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_activity);
+    }
+}
