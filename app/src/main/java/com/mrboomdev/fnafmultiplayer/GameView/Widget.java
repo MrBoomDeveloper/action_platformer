69880bcc12ea39717ee521d0b0b5a4705c08fc4e
MrBoomDeveloper <mrboomsocial@gmail.com>
2022-03-08 17:24:16 +0500

    uhm... i haven't started working at the game.

---
+++ app/src/main/java/com/mrboomdev/fnafmultiplayer/GameView/Widget.java
@@ -0,0 +1,11 @@
+package com.mrboomdev.fnafmultiplayer.GameView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Widget extends View {
+    public Widget(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
